b7e396fe2f02f791d5ca1f4a1849b75b16c9866f
kovacsbelsen <kovacsbence23@gmail.com>
2021-01-15 16:21:45 +0100

    update app_bence

--- webapp/app_bence.py
+++ webapp/app_bence.py
@@ -49,9 +49,9 @@ st.image(library)
 
 # presentation of work and data
 st.header('The project :books:')
-st.write("""The amounts of books published in the 20th century is massive. To navigate better in this sea of paper we need to collect datas and ask them some questions.
-        We took [GoodReads](https://www.goodreads.com/list/show/6.Best_Books_of_the_20th_Century) list of the best book of the 20th century as our data source,
-        created a program to scrape all the information we needed from there and saved it to a dataframe.""")
+st.write("""The amount of books published in the 20th century is massive. To navigate better in this sea of paper we need to collect data and ask them some questions.
+        We visited the website [GoodReads](https://www.goodreads.com/list/show/6.Best_Books_of_the_20th_Century), from where we extracted a list of the best books of the 20th century as our source of data,
+        by creating a program to scrape all the information we needed from the pages and we saved it to a dataframe.""")
 if st.button('Click here to see the DataFrame'):
         st.dataframe(df)
 
@@ -78,54 +78,54 @@ if st.button('Click here to see the DataFrame'):
 #(df['mean_norm_ratings'] - mmin) / (mmax - mmin) *10)""", language='python')
 
 # explorating dataframe
-st.header('Exploring the datas :mag:')
-st.write("Here you can do some explorative work on the DataFrame by showing only the column you're interested in.")
+st.header('Exploring the data :mag:')
+st.write("Here you can do some exploration of the DataFrame by showing only the columns you're interested in.")
 columns_to_show = st.multiselect("Select the columns you want to display", df.columns)
 st.dataframe(df[columns_to_show])
 
-st.write("We can furthermore explore the datas. Below there are some filtering option.")
-with st.beta_expander('Filter the datas by minum and maximum rating normalized from 0 to 10'):
+st.write("We can furthermore explore the data. See some filtering options below.")
+with st.beta_expander('Filter by minimum and maximum rating normalized from 0 to 10'):
         min_rating = st.number_input("Minimum Rating (from 0 to 10)", min_value=0, max_value=10)
         max_rating = st.number_input("Maximum Rating (from 0 to 10)", min_value=0, max_value=10, value=10)
-with st.beta_expander("Or by minimum and maximum year of publication"):
+with st.beta_expander("Filter by minimum and maximum year of publication"):
         min_year = st.number_input("Minimum Year (from 1900 to 2003)", min_value=1900)
         max_year = st.number_input("Maximum Year (from 1900 to 2003)", min_value=1900, max_value=2003, value=2003)
-with st.beta_expander("Or by minimum and maximum number of pages"):
+with st.beta_expander("Filter by minimum and maximum number of pages"):
         min_pag = st.number_input("Minimum Pages (from 23 to 14777)", min_value=23)
         max_pag = st.number_input("Maximum Pages (from 23 to 14777)", min_value=0, max_value=14777, value=14777)
-with st.beta_expander("Or by minimum and maximum number of awards received"):
+with st.beta_expander("Filter by minimum and maximum number of awards received"):
         min_award = st.number_input("Minimum Awards (from 0 to 28)", min_value=0)
         max_award = st.number_input("Maximum Awards (from 0 to 28)", min_value=0, max_value=28, value=28)
 st.dataframe(df.query("@min_rating<=minmax_norm_ratings<=@max_rating & @min_year<=pub_year<=@max_year & @min_pag<=N_pag<=@max_pag & @min_award<=Awards<=@max_award"))
 with st.beta_expander('Search by author'):
         author_input = st.text_input('Author Name', 'Author Name')
         st.dataframe(df[df['Author'].str.contains(author_input)])
-with st.beta_expander('Or by title'):
+with st.beta_expander('Search by title'):
         title_input = st.text_input('Title', 'Title')
         st.dataframe(df[df['Title'].str.contains(title_input)])
 
 # visualization
 st.header('Visualizing the data :bar_chart:')
-st.write("There are a lot of question we can ask about the best books of the 20th Century. We tried to answer some of them")
+st.write("There are a lot of questions we can ask about the best books of the 20th Century. We tried to answer some of them")
 with st.beta_expander('Which are the best rated books?'):
         st.image(best_rated_book)
 with st.beta_expander('Which are the books that won more awards?'):
         st.image(best_award_book)
-with st.beta_expander('Is there any correlation between number of pages and number of ratings?'):
+with st.beta_expander('Is there any correlation between numbers of pages and numbers of ratings?'):
         st.image(pages_rating)
-        st.write("Observing the scatter plot we can see what it looks like a little negative correlation. The correlation coefficient, however, is -0.04, meaning that this correlation is not statistically significant")
+        st.write("Observing the scatter plot we can see that it looks like there is a little negative correlation. The correlation coefficient, however, is -0.04, meaning that this correlation is not statistically significant")
 with st.beta_expander('Is there any difference between the normalized ratings and the mean ratings?'):
         st.image(norm_rating_dists)
-        st.write("Both the normalized ratings and the mean ratings follow a normal distribution. This mean we have not much book that are rated really good or really bad. It's fair to think if this follow the real value of the book or if people tend to not give extreme ratings")
+        st.write("Both the normalized ratings and the mean ratings follow a normal distribution. This means we do not have many book that are rated really good or really bad. Based on this information, it seems like people tend not to give extremely high or extremely low ratings.")
 with st.beta_expander('Are we sure the normal distribution is the best fit for these datas?'):
         st.image(best_fit)
-        st.write('We used an algorithm to find the best possible fit, and the results above confirms that the normal distribution is actually the best fit.')
+        st.write('We used an algorithm to find the best possible fit, and the results above confirm that the normal distribution is actually the best fit.')
 with st.beta_expander('Does the awards number follow any kind of distribution?'):
         st.image(book_awards)
-        st.write('We can see that we have almost an exponential decrease of books going up with the number of awards')
-with st.beta_expander('Is there any correlation between the ratings on goodreads and the awards winned by the book?'):
+        st.write('We can see that we have almost an exponential decrease of books by the increase of the number of awards')
+with st.beta_expander('Is there any correlation between the ratings on goodreads and the awards won by the book?'):
         st.image(ratings_awards)
-        st.write("The correlation is not significant, with a correlation coefficient of 0.03. Ratings are given by the internet people, while awards are usually given by professional. Having no correlation between those two things show that really often the value peoples give to a book is different than the professional judgement. Population of raters from internet people is much higher than the professionals giving awards, therefore it is more of a democratic and real outlook on how good the book is.")
+        st.write("The correlation is not significant, with a correlation coefficient of 0.03. Ratings are given by the internet people, while awards are usually given by professionals. Having no correlation between those two things show that really often the value peoples give to a book is different than the professional judgement. Population of raters from internet people is much higher than the professionals giving awards, therefore it is more of a democratic and real outlook on how good the book is.")
 with st.beta_expander('What is the probability that a book that is part of a series has won an award?'):
         st.write('We can answer this question by using the Bayes Theorem.')
         st.code("""p(award|series) = [p(series|award)*p(award)] / p(series)  --> Bayes Theorem
@@ -154,8 +154,8 @@ st.text(' ')
 st.text(' ')
 
 # function description
-st.header('How this app works? :hammer_and_wrench:')
-st.write('If you are curious about how this app works, check the dropdowns below!')
+st.header('How does this app work? :hammer_and_wrench:')
+st.write('If you are curious about how this app works, check the dropdown options below!')
 
 with st.beta_expander('How does the column selection work?'):
         st.write('The column selection tool work with a multiselect box method of Streamlit, combined with Pandas.')
@@ -164,7 +164,7 @@ st.dataframe(df[columns_to_show])""", language='python')
         st.write("We store the result of the multiselction in a variable and we call that variable as columns index when showing the dataframe.")
 
 with st.beta_expander('How do the filtering functions work?'):
-        st.write("The filtering function work with input method of Streamlit, and uses that input to filter the dataframe with Pandas")
+        st.write("The filtering functions work with input method of Streamlit, and uses that input to filter the dataframe with Pandas")
         st.code("""min_rating = st.number_input("Minimum Rating (from 0 to 10)", min_value=0, max_value=10)
 max_rating = st.number_input("Maximum Rating (from 0 to 10)", min_value=0, max_value=10, value=10)
 
